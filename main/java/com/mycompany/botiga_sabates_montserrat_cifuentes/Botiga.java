058cc230320124cff8644f8cdb892db34ce239d1
montsecg <80423945+montsecg@users.noreply.github.com>
2022-04-22 12:33:00 +0200

    Botiga-Montse2

---
+++ main/java/com/mycompany/botiga_sabates_montserrat_cifuentes/Botiga.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.botiga_sabates_montserrat_cifuentes;
+
+/**
+ *
+ * @author Usuario
+ */
+public interface Botiga {
+    
+    public boolean registre ();
+    public void registreClient ();
+    //public boolean client ();
+    //public boolean clientVip ();
+    public boolean inici ();
+    public void iniciSessio ();
+    public boolean cistella ();
+    public boolean compra ();
+    public void veureArticles ();
+    public void homeBotiga ();
+    public void tancarSessio ();
+
+}
---
+++ main/java/com/mycompany/botiga_sabates_montserrat_cifuentes/CistellaCompra.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.botiga_sabates_montserrat_cifuentes;
+
+/**
+ *
+ * @author Usuario
+ */
+public class CistellaCompra extends Client implements Botiga {
+
+    @Override
+    public boolean registre() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void registreClient() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean inici() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void iniciSessio() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean cistella() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean compra() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void veureArticles() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void homeBotiga() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void tancarSessio() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
